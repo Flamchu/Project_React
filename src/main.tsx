0c479defbc6a60d353bc67d9737a08751be4f0bd
DanielNovak <ndan@post.cz>
2024-12-03 11:27:17 +0100

    nefunguje more

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
+<<<<<<< HEAD
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import "./index.css";
 import App from "./App.tsx";
+=======
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.scss'
+import App from './App.tsx'
+>>>>>>> d9e5b5d (sex)
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
